49d08d9ae786d039da6f29724c990c6e6d24caa1
sfted <someone.fte@gmail.com>
2022-01-29 10:51:04 +0300

    Сделал свойство Data опциональным

--- TgPics.Core/Models/Picture.cs
+++ TgPics.Core/Models/Picture.cs
@@ -1,11 +1,13 @@
-﻿namespace TgPics.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TgPics.Core.Models;
 
 public class Picture
 {
     public Picture() { }
 
     public int Id { get; set; }
-    public byte[] Data { get; set; }
     public string Source { get; set; }
     public int Position { get; set; }
+    public byte[]? Data { get; set; }
 }
