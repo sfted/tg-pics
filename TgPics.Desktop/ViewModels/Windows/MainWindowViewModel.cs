c60537dbbee3acf9d35917f68834b81d7eb60c0d
sfted <someone.fte@gmail.com>
2022-02-01 16:58:20 +0300

    написал навигацию по элементам меню, добавил страницу PageNotFound

---
+++ TgPics.Desktop/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TgPics.Desktop.ViewModels.Windows;
+
+using TgPics.Desktop.MVVM;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+
+    }
+
+    public RelayCommand NavigateToCommand { get; private set; }
+
+
+}
--- TgPics.Desktop/Views/Windows/MainWindow.xaml.cs
+++ TgPics.Desktop/Views/Windows/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using Microsoft.UI.Xaml;
+﻿namespace TgPics.Desktop.Views.Windows;
+
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using TgPics.Desktop.MVVM.Interfaces;
+using TgPics.Desktop.ViewModels.Windows;
 using TgPics.Desktop.Views.Pages;
 
-namespace TgPics.Desktop.Views.Windows
+public sealed partial class MainWindow : Window, IViewModel<MainWindowViewModel>
 {
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void OnFrameLoaded(object sender, RoutedEventArgs e)
-        {
-            frame.Navigate(typeof(SettingsPage));
-        }
-
-        private void OnGridLoaded(object sender, RoutedEventArgs e)
-        {
-            App.XamlRoot = (sender as Grid).XamlRoot;
-        }
+        InitializeComponent();
+        ViewModel = new();
     }
+
+    public MainWindowViewModel ViewModel { get; set; }
+
+    private void OnFrameLoaded(object sender, RoutedEventArgs e)
+    {
+        //frame.Navigate(typeof(SettingsPage));
+    }
+
+    private void OnGridLoaded(object sender, RoutedEventArgs e)
+    {
+        App.XamlRoot = (sender as Grid).XamlRoot;
+    }
+
+    private void OnItemInvoked(
+        NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        if(args.InvokedItemContainer != null)
+            frame.Navigate(PageTagToType(args.InvokedItemContainer.Tag?.ToString()));
+    }
+
+    private static Type PageTagToType(string tag) =>
+        tag switch
+        {
+            "settings" => typeof(SettingsPage),
+            _ => typeof(PageNotFound),
+        };
 }
